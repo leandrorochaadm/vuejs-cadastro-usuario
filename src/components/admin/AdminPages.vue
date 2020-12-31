70b05f384efaaff48426fc6db58707a6d8a4d1d0
leandrorochaadm <leandrorochaadm@gmail.com>
2020-12-31 11:08:26 -0400

    criacao tabs na tela admin

--- src/components/admin/AdminPages.vue
+++ src/components/admin/AdminPages.vue
@@ -1,6 +1,18 @@
 <template>
 <div class="admin-pages">
     <PageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastros & cia" />
+    <div class="admin-pages-tabs">
+        <b-card no-body>
+            <b-tabs card>
+                <b-tab title="Artigos" active >
+                    <h1>Artigo</h1>
+                </b-tab>
+                <b-tab title="Categorias"  >
+                    <h1>Categoria</h1>
+                </b-tab>
+            </b-tabs>
+        </b-card>
+    </div>
 </div>
    
 </template>
